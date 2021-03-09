8d5b0127c6e3f8f12b760656ae787a4eddfa32b2
SahilSinghStudierende <sahil.singh98@hotmail.com>
2021-03-09 18:21:36 +0100

    Movie App: Adapt the Movie initialization

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -14,14 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Prepare Movie-Data for Data Binding
-        val movie = Movie(
-            "Irgendein Title",
-            10f,
-            listOf("Drama", "Sport"),
-            listOf("Sahil", "Thomas", "Baljinder"),
-            listOf("Oliver", "Jennifer"),
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-        )
+        val movie = initializeMovie()
+
+        // Bind Movie to Activity
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.movie = movie
@@ -32,5 +27,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun floatButtonClick() {
         Toast.makeText(this, "Float Button clicked", Toast.LENGTH_LONG).show()
+    }
+
+    private fun initializeMovie(): Movie {
+        return Movie(
+            title = "Irgendein Title",
+            rating = 10f,
+            genre = listOf("Drama", "Sport"),
+            actor = listOf("Sahil", "Thomas", "Baljinder"),
+            creator = listOf("Oliver", "Jennifer"),
+            description = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis " +
+                    "et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui " +
+                    "ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut " +
+                    "labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? " +
+                    "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        )
     }
 }
